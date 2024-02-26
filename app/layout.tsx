6396e76c3902e3e3cb639544fa0f0e9e7e9ab399
Rahul A B <rahulbushi69@gmail.com>
2024-02-26 17:13:39 +0000

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
             <body className={`${inter.className} relative pt-28 sm:pt-36 bg-gray-50 text-gray-950 h-[5000px]`}>
                 <div className="-z-10 bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
                 <div className="-z-10 bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-                <Header />
+                {/* <Header /> */}
                 {children}
             </body>
         </html>
